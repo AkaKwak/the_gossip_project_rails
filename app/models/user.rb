b3a58b0ae87b62631be371e4de3f25964b958530
AkaKwak <christophe.alaterre@gmail.com>
2024-10-31 23:49:27 +0100

    password migrate + bcrypt install

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   has_many :gossips
   has_many :comments, dependent: :destroy
+  has_secure_password
   belongs_to :city, optional: true
   validates :age, numericality: { only_integer: true, greater_than: 0 }, allow_nil: true
+  validates :password, presence: true, length: { minimum: 6 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_10_31_215703) do
+ActiveRecord::Schema[7.2].define(version: 2024_10_31_223148) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -56,6 +56,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_10_31_215703) do
     t.string "email"
     t.text "description"
     t.integer "city_id"
+    t.string "password_digest"
   end
 
   add_foreign_key "comments", "gossips"
